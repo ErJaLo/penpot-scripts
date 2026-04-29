13bd7e93b3caa02b15b3c02f345cbbbdba231392
ErJaLo <156296701+ErJaLo@users.noreply.github.com>
2026-04-29 03:03:57 +0000

    .

--- FlashCard/plugin.js
+++ FlashCard/plugin.js
@@ -1,3 +1,17 @@
 const board = penpot.createBoard();
-console.log("board fns:", Object.keys(board).filter(k => typeof board[k] === 'function'));
-console.log("board keys:", Object.keys(board));
+console.log("width descriptor:", JSON.stringify(Object.getOwnPropertyDescriptor(board, 'width')));
+console.log("x descriptor:", JSON.stringify(Object.getOwnPropertyDescriptor(board, 'x')));
+console.log("name descriptor:", JSON.stringify(Object.getOwnPropertyDescriptor(board, 'name')));
+
+// Intentar setear x e y (sabemos que funcionan desde antes)
+board.x = 100;
+board.y = 100;
+console.log("x después:", board.x);
+
+// Intentar setear width directamente
+try {
+  board.width = 500;
+  console.log("width seteado:", board.width);
+} catch(e) {
+  console.error("width error:", e.message);
+}
