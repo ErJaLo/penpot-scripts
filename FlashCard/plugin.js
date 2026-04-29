e69c6d9a7cb3888c4b8c8155e43944f53e352678
ErJaLo <156296701+ErJaLo@users.noreply.github.com>
2026-04-29 01:25:27 +0000

    231231231

--- FlashCard/plugin.js
+++ FlashCard/plugin.js
@@ -1,10 +1,9 @@
-// Ver qué métodos tiene un shape (el componente maestro en el canvas)
-const allShapes = penpot.root.children;
-console.log("root children count:", allShapes?.length);
-console.log("root children names:", allShapes?.map(c => c.name));
+const board = penpot.root.children?.find(s => s.name === "Board");
+console.log("board children names:", board?.children?.map(c => c.name));
 
-const masterShape = allShapes?.find(s => s.name === "simple");
-console.log("masterShape keys:", masterShape ? Object.keys(masterShape) : "no encontrado");
+const masterShape = board?.children?.find(s => s.name === "simple");
+console.log("masterShape encontrado:", !!masterShape);
+
 if (masterShape) {
   for (const key of Object.keys(masterShape)) {
     console.log(`masterShape.${key}:`, typeof masterShape[key]);
