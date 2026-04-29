29d4d04ed3a73ede2751e5d770553fbfbc9e3c04
ErJaLo <156296701+ErJaLo@users.noreply.github.com>
2026-04-29 01:31:07 +0000

    2313213

--- FlashCard/plugin.js
+++ FlashCard/plugin.js
@@ -1,11 +1,17 @@
 const board = penpot.root.children?.find(s => s.name === "Board");
-console.log("board children names:", board?.children?.map(c => c.name));
-
 const masterShape = board?.children?.find(s => s.name === "simple");
-console.log("masterShape encontrado:", !!masterShape);
 
-if (masterShape) {
-  for (const key of Object.keys(masterShape)) {
-    console.log(`masterShape.${key}:`, typeof masterShape[key]);
-  }
+// Ver si tiene info de componente
+console.log("mainComponent:", masterShape?.mainComponent);
+console.log("componentId:", masterShape?.componentId);
+console.log("mainInstance:", masterShape?.mainInstance);
+
+// Ver si penpot.selection tiene métodos útiles
+console.log("selection keys:", Object.keys(penpot.selection));
+console.log("selection:", JSON.stringify(penpot.selection));
+
+// Ver si history tiene duplicate
+console.log("history keys:", Object.keys(penpot.history));
+for (const key of Object.keys(penpot.history)) {
+  console.log(`history.${key}:`, typeof penpot.history[key]);
 }
